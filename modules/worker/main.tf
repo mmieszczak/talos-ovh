f2d7739277c65f71f130e51928abdd3357974739
Michał Mieszczak <m.mieszczak@piwik.pro>
2024-07-26 09:58:08 +0200

    Add worker node module

---
+++ modules/worker/main.tf
@@ -0,0 +1,102 @@
+variable "name" {
+  type = string
+}
+
+variable "node_count" {
+  type = number
+}
+
+variable "image_id" {
+  type = string
+}
+
+variable "flavor" {
+  type = string
+}
+
+variable "user_data" {
+  type = string
+}
+
+variable "network" {
+  type = string
+}
+
+variable "client_configuration" {
+  type = map(any)
+}
+
+variable "controller_address" {
+  type = string
+}
+
+variable "node_labels" {
+  type    = map(string)
+  default = {}
+}
+
+variable "node_taints" {
+  type    = map(string)
+  default = {}
+}
+
+data "openstack_compute_flavor_v2" "flavor" {
+  name = var.flavor
+}
+
+resource "openstack_blockstorage_volume_v3" "worker" {
+  count    = var.node_count
+  region   = "WAW1"
+  name     = "${var.name}-${count.index}"
+  size     = 50
+  image_id = var.image_id
+
+  lifecycle {
+    ignore_changes = [
+      image_id,
+    ]
+  }
+}
+
+resource "openstack_compute_instance_v2" "worker" {
+  count               = var.node_count
+  name                = "${var.name}-${count.index}"
+  security_groups     = ["default"]
+  flavor_id           = data.openstack_compute_flavor_v2.flavor.id
+  user_data           = var.user_data
+  stop_before_destroy = true
+
+  block_device {
+    uuid             = openstack_blockstorage_volume_v3.worker[count.index].id
+    source_type      = "volume"
+    boot_index       = 0
+    destination_type = "volume"
+  }
+
+  network {
+    name = var.network
+  }
+
+  lifecycle {
+    ignore_changes = [
+      user_data,
+    ]
+  }
+}
+
+resource "talos_machine_configuration_apply" "worker" {
+  count                       = var.node_count
+  client_configuration        = var.client_configuration
+  machine_configuration_input = var.user_data
+  node                        = openstack_compute_instance_v2.worker[count.index].network[0].fixed_ip_v4
+  endpoint                    = var.controller_address
+
+  config_patches = [
+    yamlencode({
+      machine = {
+        nodeLabels = var.node_labels,
+        nodeTaints = var.node_taints,
+      }
+    })
+  ]
+}
